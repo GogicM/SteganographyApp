3f9f1eb5d195e5bee0d328db850216bd19e2682d
GogicM <milan.gogic@comtrade.com>
2018-01-22 17:07:27 +0100

    Encode / decode added

--- src/controllers/UserPanelController.java
+++ src/controllers/UserPanelController.java
@@ -1,7 +1,10 @@
 package controllers;
 
 import java.awt.Desktop;
+import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferByte;
+import java.awt.image.WritableRaster;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
@@ -41,6 +44,7 @@ import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import javafx.stage.Window;
 import server.ServerThread;
+import steganography.Steganography;
 
 public class UserPanelController {
 
@@ -49,6 +53,8 @@ public class UserPanelController {
     private String userName;
     private String selectedUsername;
     private BufferedImage image = null;
+    private Steganography steganography;
+    private File imgFile;
     private final Desktop desktop = Desktop.getDesktop();
 
     private static final int PORT_NUMBER = 9999;
@@ -77,7 +83,8 @@ public class UserPanelController {
     private Label newMessagesLabel;
     @FXML
     private void initialize() {
-
+    	
+    	steganography = new Steganography();
     	viewNewMessages.setVisible(false);
     	try {
     		File f = new File("src/controllers/users.txt");
@@ -109,138 +116,31 @@ public class UserPanelController {
 
     }
 
-//    @FXML
-//    protected void handleSaveButton(ActionEvent event) {
-//        if (!tArea.isVisible()) {
-//            alert("You have to modify file in order to save it!");
-//        } else {
-//            try {
-//                SignInController.oos.writeObject("");
-//                String option = "modify";
-//                String signature = SignInController.asymmetricCrypto.signMessagge(option, SignInController.privateKey);
-//                String encOption = SignInController.asymmetricCrypto.EncryptStringAsymmetric(option, SignInController.serverPublicKey);
-//                SignInController.oos.writeObject(new String[]{signature, encOption});
-//                String encData = SignInController.asymmetricCrypto.EncryptStringSymmetric(tArea.getText(), SignInController.sessionKey);
-//                SignInController.oos.writeObject(new String[] {SignInController.asymmetricCrypto.signMessagge(tArea.getText(), SignInController.privateKey) ,encData});
-//                String[] signatureAndStatus = (String[]) SignInController.ois.readObject();
-//                String status = SignInController.asymmetricCrypto.DecryptStringSymmetric(signatureAndStatus[1], SignInController.sessionKey);
-//                if(!SignInController.asymmetricCrypto.verifyDigitalSignature(status, signatureAndStatus[0], SignInController.serverPublicKey)) {
-//                	alert("Intrusion occured! Exiting application...");
-//                	System.exit(0);
-//                }
-//                if ("true".equals(status)) {
-//                    alert("You successfully edited file");
-//                } else {
-//                    alert("File can not be edited");
-//                }
-//                tArea.setVisible(false);
-//            } catch (Exception ex) {
-//                Logger.getLogger(UserPanelController.class.getName()).log(Level.SEVERE, null, ex);
-//            }
-//        }
-//
-//    }
-
-//    @FXML
-//    protected void handleEditButton(ActionEvent event) {
-//
-//        tArea.setVisible(true);
-//        String content = tArea.getText();
-//        try {
-//            SignInController.oos.writeObject("");
-//            String option = "edit";
-//            String encOption = SignInController.asymmetricCrypto.EncryptStringAsymmetric(option, SignInController.serverPublicKey);
-//            String signature = SignInController.asymmetricCrypto.signMessagge(option, SignInController.privateKey);
-//            SignInController.oos.writeObject(new String[]{signature, encOption});
-//            String encFileName = SignInController.asymmetricCrypto.EncryptStringSymmetric(fileName, SignInController.sessionKey);
-//            SignInController.oos.writeObject(new String[] {SignInController.asymmetricCrypto.signMessagge(fileName, SignInController.privateKey), encFileName });
-//            String[] signatureAndContent = (String[]) SignInController.ois.readObject();
-//            String contentFromServer = SignInController.asymmetricCrypto.DecryptStringSymmetric(signatureAndContent[1], SignInController.sessionKey);
-//            if(!SignInController.asymmetricCrypto.verifyDigitalSignature(contentFromServer, signatureAndContent[0], SignInController.serverPublicKey)) {
-//            	alert("Intrusion occured! Exiting application...");
-//            	System.exit(0);
-//            }
-//            tArea.setText(contentFromServer);
-//        } catch (Exception ex) {
-//            Logger.getLogger(UserPanelController.class.getName()).log(Level.SEVERE, null, ex);
-//        }
-//
-//    }
-
-//    @FXML
-//    protected void handleShowLogsButton(ActionEvent event) {
-//        try {
-//            String option = "logs";
-//            String encOption = SignInController.asymmetricCrypto.EncryptStringAsymmetric(option, SignInController.serverPublicKey);
-//            String signature = SignInController.asymmetricCrypto.signMessagge(option, SignInController.privateKey);
-//
-//            SignInController.oos.writeObject("");
-//            SignInController.oos.writeObject(new String[]{signature, encOption});
-//            String[] logsAndSignatureFromServer = (String[]) SignInController.ois.readObject();
-//            String logsFromServer = new String(SignInController.asymmetricCrypto.DecryptStringSymmetric(logsAndSignatureFromServer[1], SignInController.sessionKey));
-//            if(!SignInController.asymmetricCrypto.verifyDigitalSignature(logsFromServer, logsAndSignatureFromServer[0], SignInController.serverPublicKey)) {
-//            	alert("Intrusion has occured! Exiting application...");
-//            	System.exit(0);
-//            }
-//            logs.setText(logsFromServer);
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//        logs.setVisible(true);
-//    }
-
-//    @FXML
-//    protected void handleUploadNewFile(ActionEvent event) {
-//        if (newFileContent.getText().isEmpty()) {
-//            alert("You can't upload empty file!");
-//        } else {
-//            FXMLLoader loader = new FXMLLoader(getClass().getClassLoader().getResource("fxml/uploadNewFileForm.fxml"));
-//            try {
-//                newFileData = newFileContent.getText();
-//                Parent root = (Parent) loader.load();
-//
-//                SendNewMessageController controller = loader.getController();
-//
-//                Stage stage = new Stage();
-//                stage.setTitle(" User panel");
-//                stage.setScene(new Scene(root));
-//                stage.show();
-//        stage.hide();
-//            } catch (IOException e) {
-//                e.printStackTrace();
-//            }
-//        }
-//    }
-
-
-//    @FXML
-//    protected void handleBrowseButton(ActionEvent event) {
-//        FileChooser fileChooser = new FileChooser();
-//        configureFileChooser(fileChooser);
-//        File file = new File("src/certificates");
-//        if (file.exists()) {
-//            //bug in FileChooser, one must set initial directory or it will throw exception
-//            fileChooser.setInitialDirectory(file);
-//        }
-//        file = fileChooser.showOpenDialog(SignInController.stage1);
-//        if(true) { //mokup condition, when I complete it, it will check can message fit in image
-//        	alert("Message can not fit in selected image.");
-//        	sendMessageButton.setVisible(false);
-//        }
-//        if (messageField.getText() != null ) {
-//        	sendMessageButton.setVisible(true);
-//        }
-//    }
     
     @FXML
     protected void handleSendMessageButton(ActionEvent e) {
     	
+    	int messageLength = writeNewMessage.getText().getBytes().length;
+    	System.out.println("MESSAGE NAME : " + imgFile.getName());
+    	long imageSize = steganography.getImagesSize(imgFile.getName());
+    	//for(int i = 0; i < imagesSize.length; i++) {
+	    	if(messageLength < imageSize / 100 ) {
+	    		//do logic for  adding message to picture
+	    		//encodeText(imageToByte(image), );
+	    		if(steganography.encode("src/images", "girl", "jpg", "girl_steg", writeNewMessage.getText())) {
+	    			alert("You succesfuly encoded text!");
+	    		}
+	    	} else {
+	    		alert("Your message is too large for selected image");
+	    	}
     }
     
     @FXML
     protected void handleShowNewMessagesButton(ActionEvent e) {
     	newMessagesLabel.setVisible(false);
-    	viewNewMessages.setVisible(false);
+    	viewNewMessages.setVisible(true);
+    	viewNewMessages.setText(steganography.decode("src/images", "girl_steg"));
+    	
     }
     
     @FXML
@@ -248,98 +148,26 @@ public class UserPanelController {
     	if(!writeNewMessage.getText().isEmpty()) {
 	    	FileChooser fileChooser = new FileChooser();
 	    	configureFileChooser(fileChooser);
-	    	File file = new File("src/images");
-	    	if(file.exists()) {
-	    		fileChooser.setInitialDirectory(file);
+	    	imgFile = new File("src/images");
+	    	if(imgFile .exists()) {
+	    		fileChooser.setInitialDirectory(imgFile);
 	    	}
-	    	file = fileChooser.showOpenDialog(SignInController.stage1);
-	    	try {
-	    		System.out.println(file.getName());
-				image = ImageIO.read(file);
-			} catch (IOException e1) {
-				// TODO Auto-generated catch block
-				e1.printStackTrace();
-			}
-	    	int messageLength = writeNewMessage.getText().getBytes().length;
-	    	System.out.println("MESSAGE SIZE : " + messageLength);
-	    	long imageSize = getImagesSize(file.getName());
-	    	//for(int i = 0; i < imagesSize.length; i++) {
-		    	if(messageLength < imageSize / 100 ) {
-		    		//do logic for  adding message to picture
-		    		storeEncryptedMessageInPicture();
-		    		
-		    	} else {
-		    		alert("Your message is too large for selected image");
-		    	}
+	    	imgFile = fileChooser.showOpenDialog(SignInController.stage1);
+//	    	try {
+	    		System.out.println(imgFile.getName());
+//				image = ImageIO.read(file);
+//			} catch (IOException e1) {
+//				// TODO Auto-generated catch block
+//				e1.printStackTrace();
+//			}
+
 	    	//}
     	} else {
     		alert("Write message first!");
     	}
     }
     
-    private void storeEncryptedMessageInPicture() {
-		// TODO Auto-generated method stub
-		
-	}
     
-    private byte[] encodeText(byte[] image, byte[] addition, int offset)
-    {
-    	if(addition.length + offset > image.length)
-    	{
-    		throw new IllegalArgumentException("File not long enough!");
-    	}
-    	for(int i=0; i<addition.length; ++i)
-     	{
-    		int add = addition[i];
-    		for(int bit=7; bit>=0; --bit, ++offset)
-    		{
-    	   	 	int b = (add >>> bit) & 1;
-    	   		 image[offset] = (byte)((image[offset] & 0xFE) | b );
-    		}
-    	}
-    	return image;
-    }
-    
-	private byte[] decodeText(byte[] image)
-	{
-		int length = 0;
-		int offset  = 32;
-		//loop through 32 bytes of data to determine text length
-		for(int i=0; i<32; ++i) //i=24 will also work, as only the 4th byte contains real data
-		{
-			length = (length << 1) | (image[i] & 1);
-		}
-		
-		byte[] result = new byte[length];
-		
-		//loop through each byte of text
-		for(int b=0; b<result.length; ++b )
-		{
-			//loop through each bit within a byte of text
-			for(int i=0; i<8; ++i, ++offset)
-			{
-				//assign bit: [(new byte value) << 1] OR [(text byte) AND 1]
-				result[b] = (byte)((result[b] << 1) | (image[offset] & 1));
-			}
-		}
-		return result;
-	}
-	
-	private long getImagesSize(String imageName) {
-    	File f = new File("src/images/" + imageName);
-    	//String[] fileNames = f.list();
-    	long imageSize;
-    	int i = 0;
-    	
-    	//for(String s : fileNames) {
-    	//	File file = new File("src/images/" + s);
-    		System.out.println("FILE NAME : " + f.getName());
-    		imageSize = f.length();
-    		System.out.println("IMAGE SIZE : " + imageSize);
-    		//i++;
-    	
-    	return imageSize;
-    }
 
 	private static void configureFileChooser(final FileChooser fileChooser) {
 
@@ -362,5 +190,7 @@ public class UserPanelController {
 
         alert.showAndWait();
     }
+    
+ 
 
 }
---
+++ src/steganography/Steganography.java
@@ -0,0 +1,171 @@
+package steganography;
+
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferByte;
+import java.awt.image.WritableRaster;
+import java.io.File;
+
+import javax.imageio.ImageIO;
+
+public class Steganography {
+	
+   public byte[] encodeText(byte[] image, byte[] addition, int offset) {
+	   
+	   if(addition.length + offset > image.length) {
+		   throw new IllegalArgumentException("File not long enough!");
+	   }
+	   for(int i=0; i<addition.length; ++i) {
+		   int add = addition[i];
+		   for(int bit=7; bit>=0; --bit, ++offset) {
+			   int b = (add >>> bit) & 1;
+    	   	   image[offset] = (byte)((image[offset] & 0xFE) | b );
+		   }
+	   }
+	   return image;
+    }
+    
+	public byte[] decodeText(byte[] image) {
+		
+		int length = 0;
+		int offset  = 32;
+		//loop through 32 bytes of data to determine text length
+		for(int i=0; i<32; ++i) { //i=24 will also work, as only the 4th byte contains real data
+			length = (length << 1) | (image[i] & 1);
+		}
+		
+		byte[] result = new byte[length];
+		
+		//loop through each byte of text
+		for(int b=0; b<result.length; ++b ) {
+			//loop through each bit within a byte of text
+			for(int i=0; i<8; ++i, ++offset) {
+				//assign bit: [(new byte value) << 1] OR [(text byte) AND 1]
+				result[b] = (byte)((result[b] << 1) | (image[offset] & 1));
+			}
+		}
+		return result;
+	}
+	
+	public long getImagesSize(String imageName) {
+    	File f = new File("src/images/" + imageName);
+    	//String[] fileNames = f.list();
+    	long imageSize;
+    	int i = 0;
+    	
+    	//for(String s : fileNames) {
+    	//	File file = new File("src/images/" + s);
+    		System.out.println("FILE NAME : " + f.getName());
+    		imageSize = f.length();
+    		System.out.println("IMAGE SIZE : " + imageSize);
+    		//i++;
+    	
+    	return imageSize;
+    }
+	
+	public byte[] imageToByte(BufferedImage image) {
+	    	byte[] imageBytes;
+	    	
+	    	WritableRaster raster = image.getRaster();
+	    	DataBufferByte buffer = (DataBufferByte) raster.getDataBuffer();
+	    	imageBytes = buffer.getData();
+	    	
+	    	return imageBytes;
+	    	
+	    }
+	
+	public boolean encode(String path, String original, String ext1, String stegan, String message) {
+		
+			String file_name = createPathToImage(path,original,ext1);
+			System.out.println(file_name);
+			BufferedImage image_orig = getImage(file_name);
+			
+			//user space is not necessary for Encrypting
+			BufferedImage image = user_space(image_orig);
+			image = add_text(image,message);
+			
+			return(setImage(image,new File(createPathToImage(path,stegan,"jpg")),"jpg"));
+	}
+		
+	public String decode(String path, String name) {
+			
+		byte[] decode;
+		try {
+			//user space is necessary for decrypting
+			BufferedImage image  = user_space(getImage(createPathToImage(path,name,"jpg")));
+			decode = decodeText(imageToByte(image));
+			return(new String(decode));
+		} catch(Exception e) {
+				e.printStackTrace();
+				return "";
+			}
+	}
+	
+	public String createPathToImage(String path, String name, String ext) {
+		return path + "/" + name + "." + ext;
+	}
+		
+	private BufferedImage add_text(BufferedImage image, String text) {
+		//convert all items to byte arrays: image, message, message length
+		byte img[] = imageToByte(image);
+		byte msg[] = text.getBytes();
+		byte len[] = intToByteConversion(msg.length);
+		try {
+			encodeText(img, len,  0); //0 first positiong
+			encodeText(img, msg, 32); //4 bytes of space for length: 4bytes*8bit = 32 bits
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		return image;
+	}
+		
+	public BufferedImage getImage(String f) {
+		BufferedImage image	= null;
+		File file = new File(f);
+			
+		try {
+			image = ImageIO.read(file);
+		} catch(Exception ex) {
+			ex.printStackTrace();
+		}
+		return image;
+	}
+		
+	public BufferedImage user_space(BufferedImage image) {
+			
+		//create new_img with the attributes of image
+		BufferedImage new_img  = new BufferedImage(image.getWidth(), image.getHeight(), BufferedImage.TYPE_3BYTE_BGR);
+		Graphics2D	graphics = new_img.createGraphics();
+		graphics.drawRenderedImage(image, null);
+		graphics.dispose(); //release all allocated memory for this image
+		return new_img;
+	}
+		
+	public byte[] intToByteConversion(int i) {
+		//originally integers (ints) cast into bytes
+		//byte byte7 = (byte)((i & 0xFF00000000000000L) >>> 56);
+		//byte byte6 = (byte)((i & 0x00FF000000000000L) >>> 48);
+		//byte byte5 = (byte)((i & 0x0000FF0000000000L) >>> 40);
+		//byte byte4 = (byte)((i & 0x000000FF00000000L) >>> 32);
+			
+		//only using 4 bytes
+		byte byte3 = (byte)((i & 0xFF000000) >>> 24); //0
+		byte byte2 = (byte)((i & 0x00FF0000) >>> 16); //0
+		byte byte1 = (byte)((i & 0x0000FF00) >>> 8 ); //0
+		byte byte0 = (byte)((i & 0x000000FF)	   );
+		//{0,0,0,byte0} is equivalent, since all shifts >=8 will be 0
+		return(new byte[]{byte3,byte2,byte1,byte0});
+	}
+		
+		
+		
+	public boolean setImage(BufferedImage image, File file, String ext) {
+		try {
+			file.delete(); //delete resources used by the File
+			ImageIO.write(image,ext,file);
+			return true;
+		} catch(Exception e) {
+			return false;
+		}
+	}
+}
